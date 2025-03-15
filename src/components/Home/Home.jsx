7af3fbeaa7bc5638011607f44f5024e275e6f20b
Juan David Marulanda <juanmarucasta@gmail.com>
2025-03-15 18:07:11 -0500

    mejorar estilos

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { Suspense } from 'react';
 import Model3D from './Model3D';
+import { ReactTyped as Typed } from 'react-typed';
 import './Home.css';
 
 const Home = () => {
@@ -10,7 +11,16 @@ const Home = () => {
     <section className="home" id="home">
       <div className="home-content">
         <h1>Hi, It's <span>Juan</span></h1>
-        <h3 className="text-animation">I'm a <span></span></h3>
+        <h3 className="text-animation">
+          I'm a <span>
+            <Typed
+              strings={['Developer','Full Stack', 'Back End', 'Front End']}
+              typeSpeed={40}
+              backSpeed={50}
+              loop
+            />
+          </span>
+        </h3>
         <p>
           Software Analysis and Development apprentice at SENA, passionate about creating innovative and useful
           technology solutions. I specialize in frontend development with Angular and enjoy continuous learning
@@ -18,11 +28,10 @@ const Home = () => {
         </p>
 
         <div className="social-icons">
-          <a href="https://www.linkedin.com"><i className='bx bxl-linkedin-square'></i></a>
-          <a href="https://github.com"><i className='bx bxl-github'></i></a>
-          <a href="https://wa.me/"><i className='bx bxl-whatsapp'></i></a>
+          <a href="https://www.linkedin.com/in/juan-david-marulanda-0b438a346/" target="_blank" rel="noopener noreferrer"><i className='bx bxl-linkedin-square'></i></a>
+          <a href="https://github.com/JuanDavid2221" target="_blank" rel="noopener noreferrer"><i className='bx bxl-github'></i></a>
           <a href="mailto:juanmarucasta@gmail.com"><i className='bx bxl-gmail'></i></a>
-          <a href="https://instagram.com"><i className='bx bxl-instagram'></i></a>
+          <a href="https://instagram.com/jmarulandad" target="_blank" rel="noopener noreferrer"><i className='bx bxl-instagram'></i></a>
         </div>
         <div className="btn-group">
           <a href="#" className='btn'>Hire</a>
@@ -38,7 +47,7 @@ const Home = () => {
             <directionalLight position={[-10, -10, -5]} intensity={2} />
             <pointLight position={[0, 10, 0]} intensity={2} />
             <Model3D />
-            <OrbitControls />
+            <OrbitControls enableZoom={false} />
           </Suspense>
         </Canvas>
       </div>
